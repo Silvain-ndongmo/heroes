8de8b0fa9af532ecea4fedd4bca610a66fae664f
silvain ndongmo <decozar457@gmail.com>
2021-05-03 12:12:17 -0400

    first commit

---
+++ src/app/hero.service.ts
@@ -0,0 +1,23 @@
+import { Injectable } from '@angular/core';
+import {Hero} from './hero';
+import { HeroesComponent } from './heroes/heroes.component';
+import {HEROES} from './mock-heroes';
+import {Observable, of } from 'rxjs';
+import {MessageService} from './message.service';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class HeroService {
+  constructor(private messageService: MessageService) { }
+getHeroes(): Observable<Hero[]> {
+  const heroes = of (HEROES);
+  this.messageService.add('HeroService: fetched heroes');
+  return heroes;
+}
+getHero(id: number): Observable<Hero> {
+  const hero = HEROES.find(h => h.id === id) as Hero;
+  this.messageService.add('HeroServcie: fetched hero id=${id}');
+  return of(hero);
+}
+}
