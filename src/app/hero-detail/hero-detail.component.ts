81b8b9372619378842347fa895d7e55d9d37831e
silvain ndongmo <decozar457@gmail.com>
2021-05-05 23:47:35 -0400

    second commit

--- src/app/hero-detail/hero-detail.component.ts
+++ src/app/hero-detail/hero-detail.component.ts
@@ -1,37 +1,38 @@
 import { Component, OnInit } from '@angular/core';
-import {Hero} from '../hero';
-import { HeroesComponent } from '../heroes/heroes.component';
-import {ActivatedRoute} from '@angular/router';
-import { Location} from '@angular/common';
-import {HeroService} from '../hero.service';
+import { ActivatedRoute } from '@angular/router';
+import { Location } from '@angular/common';
+
+import { Hero } from '../hero';
+import { HeroService } from '../hero.service';
 
 @Component({
   selector: 'app-hero-detail',
   templateUrl: './hero-detail.component.html',
-  styleUrls: ['./hero-detail.component.css']
+  styleUrls: [ './hero-detail.component.css' ]
 })
-
 export class HeroDetailComponent implements OnInit {
-   hero?: Hero;
+  hero!: Hero;
 
   constructor(
     private route: ActivatedRoute,
     private heroService: HeroService,
     private location: Location
-
-
-  ) { }
+  ) {}
 
   ngOnInit(): void {
     this.getHero();
   }
-  getHero(): void {
-    const id = Number(this.route.snapshot.paramMap.get('id'));
-    this.heroService.getHero(id)
-    .subscribe((hero: Hero | undefined) => this.hero = hero);
+  // tslint:disable-next-line: typedef
+  getHero() {
+    throw new Error('Method not implemented.');
   }
+
   goBack(): void {
     this.location.back();
   }
 
+  save(): void {
+    this.heroService.updateHero(this.hero)
+      .subscribe(() => this.goBack());
+  }
 }
